44beb0ea7ac3939c7d1aa6b01cc6b90fb959afa8
Jonas Vestby <jonves@prio.org>
2024-01-10 11:32:06 +0100

    fixed minor issues

--- R/cshapes.R
+++ R/cshapes.R
@@ -1,5 +1,6 @@
 #' Modifies certain codings of the cShapes dataset
 #'
+#' @description
 #' See each parameter for the modifications. The point of these modification is not that they
 #' are clearly better ways to code the state system. They just represent alternative ways
 #' to impose a strict system on what is arguably much more fluent and complex.
@@ -51,11 +52,15 @@
 #'  territories have no state control according to cShapes. In certain applications, having full
 #'  temporal contiguity is very useful. So this is a practical code change, more than anything.
 #'  (E.g., Ukraine celebrates their independence day on 24 August, not 26 December.)
+#' @returns sf tibble with all country borders over time
+#' @export
+#'
 #' @examples
 #' gw_mod <- cshp_gw_modifications()
 #' gw <- cshapes::cshp(useGW = TRUE)
 #' gw |> dplyr::filter(country_name %in% c("Morocco", "Spanish Sahara", "Western Sahara"))
 #' gw_mod |> dplyr::filter(country_name %in% c("Morocco", "Spanish Sahara", "Western Sahara"))
+#'
 cshp_gw_modifications <- function(western_sahara = TRUE,
                                morocco_protectorate = TRUE,
                                palestine = TRUE,
@@ -66,16 +71,16 @@ cshp_gw_modifications <- function(western_sahara = TRUE,
 
   if(morocco_protectorate){
     # gw |> dplyr::filter(status == "N/A")
-    gw <- gw |> dplyr::mutate(status = dplyr::if_else(status == "N/A", "protectorate", status))
+    gw <- gw |> dplyr::mutate(status = dplyr::if_else(.data$status == "N/A", "protectorate", .data$status))
   }
 
   if(western_sahara){
     gw <- gw |>
-      dplyr::mutate(end = dplyr::if_else(country_name == "Morocco" & fid == 441, lubridate::ymd("2019-12-31"), end)) |>
-      dplyr::filter(!(gwcode == 600 & start > lubridate::ymd("1975-11-13"))) # Remove subsequent coding (442, 443)
+      dplyr::mutate(end = dplyr::if_else(.data$country_name == "Morocco" & .data$fid == 441, lubridate::ymd("2019-12-31"), .data$end)) |>
+      dplyr::filter(!(.data$gwcode == 600 & .data$start > lubridate::ymd("1975-11-13"))) # Remove subsequent coding (442, 443)
 
     # Spanish Sahara becomes Western Sahara and is occupied by Morocco
-    western_sahara <- gw |> dplyr::filter(gwcode == 609)
+    western_sahara <- gw |> dplyr::filter(.data$gwcode == 609)
     western_sahara$start <- lubridate::ymd("1975-11-14")
     western_sahara$end <- lubridate::ymd("2019-12-31")
     western_sahara$country_name <- "Western Sahara"
@@ -88,7 +93,7 @@ cshp_gw_modifications <- function(western_sahara = TRUE,
 
   if(palestine){
     #### Israel without acknowledging occupations after the Palestinian Civil War ####
-    west_bank_and_gaza <- gw |> dplyr::filter(country_name == "West Bank") # This does not include Gaza territory!
+    west_bank_and_gaza <- gw |> dplyr::filter(.data$country_name == "West Bank") # This does not include Gaza territory!
     west_bank_and_gaza$country_name <- "Palestine"
     west_bank_and_gaza$start <- lubridate::ymd("1967-06-10")
     west_bank_and_gaza$end <- lubridate::ymd("2019-12-31")
@@ -96,15 +101,15 @@ cshp_gw_modifications <- function(western_sahara = TRUE,
     west_bank_and_gaza$gwcode <- 699
     west_bank_and_gaza$fid <- 700
 
-    gw <- gw |> dplyr::mutate(gwcode = dplyr::if_else(country_name == "West Bank", 699, gwcode))
-    gw <- gw |> dplyr::mutate(gwcode = dplyr::if_else(country_name == "Gaza", 699, gwcode))
+    gw <- gw |> dplyr::mutate(gwcode = dplyr::if_else(.data$country_name == "West Bank", 699, .data$gwcode))
+    gw <- gw |> dplyr::mutate(gwcode = dplyr::if_else(.data$country_name == "Gaza", 699, .data$gwcode))
 
     gw <- dplyr::bind_rows(gw, west_bank_and_gaza)
 
-    gw <- gw |> dplyr::mutate( end = dplyr::if_else(country_name == "Israel" & fid == 512, lubridate::ymd("2019-12-31"), end)) |>
-      dplyr::filter(!(gwcode == 666 & fid > 512)) # Not code Israels occupations as part of Israel
-    gw <- gw |> dplyr::mutate( end = dplyr::if_else(country_name == "Egypt" & fid == 492, lubridate::ymd("2019-12-31"), end)) |>
-      dplyr::filter(!(gwcode == 651 & start > lubridate::ymd("1967-06-09"))) # Not code Israel as owner of Sinai during occupation
+    gw <- gw |> dplyr::mutate( end = dplyr::if_else(.data$country_name == "Israel" & .data$fid == 512, lubridate::ymd("2019-12-31"), .data$end)) |>
+      dplyr::filter(!(.data$gwcode == 666 & .data$fid > 512)) # Not code Israels occupations as part of Israel
+    gw <- gw |> dplyr::mutate( end = dplyr::if_else(.data$country_name == "Egypt" & .data$fid == 492, lubridate::ymd("2019-12-31"), .data$end)) |>
+      dplyr::filter(!(.data$gwcode == 651 & .data$start > lubridate::ymd("1967-06-09"))) # Not code Israel as owner of Sinai during occupation
 
   }
 
@@ -113,17 +118,29 @@ cshp_gw_modifications <- function(western_sahara = TRUE,
     #gw |> dplyr::filter(end == as.Date("1991-12-20"))
     #gw |> dplyr::filter(start == as.Date("1991-12-21"))
     # Let Soviet survive 5 more days. (This deviates from GW list, but so do cShapes)
-    gw <- gw |> dplyr::mutate(end = dplyr::if_else(end == as.Date("1991-12-20"), as.Date("1991-12-25"), end))
-    gw <- gw |> dplyr::mutate(start = dplyr::if_else(start == as.Date("1991-12-21"), as.Date("1991-12-26"), start))
+    gw <- gw |> dplyr::mutate(end = dplyr::if_else(.data$end == as.Date("1991-12-20"), as.Date("1991-12-25"), .data$end))
+    gw <- gw |> dplyr::mutate(start = dplyr::if_else(.data$start == as.Date("1991-12-21"), as.Date("1991-12-26"), .data$start))
   }
 
   gw$gwcode <- as.numeric(gw$gwcode)
   return(gw)
 }
 
-
-territorial_dependencies_base <- function(...){
-  gw <- do.call(cshp_gw_modifications, list(...))
+#' Creates a network graph of territorial dependencies from the cShapes Gleditsch-Ward data
+#'
+#' @description
+#' Any country that contains or overlaps with another country in space, and is adjacent (1 day) in time
+#' are defined as neighbors. From this list of neighbors, a network graph is built.
+#'
+#' @param gw The cShapes data based on Gleditsch-Ward.
+#'  Using cshp_gw_modifications() with soviet_25dec = TRUE is recommended.
+#' @returns An igraph network graph
+#'
+#' @examples
+#' gw <- cshp_gw_modifications(soviet_25dec = TRUE)
+#' g <- territorial_dependencies(gw)
+#'
+territorial_dependencies_base <- function(gw){
   gw$uid <- paste(gw$gwcode, gw$fid, sep = "-")
 
   g <- igraph::make_empty_graph()
@@ -141,15 +158,15 @@ territorial_dependencies_base <- function(...){
   sf::sf_use_s2(FALSE)
   for(i in 1:nrow(gw)){
     obs <- gw[i,]
-    res <- gw |> dplyr::filter(((start - obs$end) == 1) | ((obs$start - end) == 1))
+    res <- gw |> dplyr::filter(((.data$start - obs$end) == 1) | ((obs$start - .data$end) == 1))
     contains <- sf::st_contains(obs, res, sparse = F)
     overlaps <- sf::st_overlaps(obs, res, sparse = F)
     idx <- contains | overlaps
-    res <- res[idx,] |> dplyr::filter( (uid != obs$uid) )
+    res <- res[idx,] |> dplyr::filter( (.data$uid != obs$uid) )
 
     if(nrow(res) > 0){
-      earlier <- res |> dplyr::filter(start < obs$start)
-      later <- res |> dplyr::filter(start >= obs$end)
+      earlier <- res |> dplyr::filter(.data$start < obs$start)
+      later <- res |> dplyr::filter(.data$start >= obs$end)
 
       if(nrow(earlier) > 0){
 
@@ -178,7 +195,7 @@ territorial_dependencies_base <- function(...){
     }
   }
 
-  edges <- edges |> distinct()
+  edges <- edges |> dplyr::distinct()
 
   sg <- edges |> igraph::graph_from_data_frame()
 
@@ -195,10 +212,11 @@ territorial_dependencies_base <- function(...){
   return(g)
 }
 
-#' Creates a dependency graph of states that have shared territory
+#' @describeIn territorial_dependencies_base Creates a network graph of territorial dependencies from the cShapes Gleditsch-Ward data
 #'
-#' See poldat::cshp_gw_modifications for descriptions of parameters.
-territorial_dependencies <- function(...){
-  do.call(territorial_dependencies_base, list(...)) |> suppressMessages()
+#' @export
+territorial_dependencies <- function(gw){
+  territorial_dependencies_cached <- memoise::memoise(territorial_dependencies_base, cache = cd)
+  suppressMessages(territorial_dependencies_cached(gw))
 }
 
