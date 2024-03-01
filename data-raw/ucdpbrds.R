65e8f2abcc4912b7a60291478b134f9f78fe8b79
Jonas Vestby <jonves@prio.org>
2024-03-01 14:52:45 +0100

    data update

--- data-raw/ucdpbrds.R
+++ data-raw/ucdpbrds.R
@@ -50,8 +50,7 @@ res <- dplyr::bind_rows(brd_single, brd_many) |>
             bdeadbes = sum(bdeadbes, na.rm = T))
 
 gw <- cshp_gw_modifications(western_sahara = TRUE, morocco_protectorate = TRUE, palestine = TRUE, soviet_25dec = TRUE)
-ged <- gw_ged(western_sahara = TRUE, morocco_protectorate = TRUE, palestine = TRUE, soviet_25dec = TRUE,
-              version = "23.1", time_interval = "year", state_base = TRUE, non_state = FALSE, one_sided = FALSE,
+ged <- gw_ged(version = "23.1", time_interval = "year", state_base = TRUE, non_state = FALSE, one_sided = FALSE,
               drop_poor_precision = FALSE, test = FALSE)
 
 ged <- ged |>
