9b61df967e99b68c42875a8c53a639cddfd6a090
Jonas Vestby <jonasvestby@Jonass-MBP.home>
2024-06-11 09:17:55 +0200

    include regime duration, fixed aggregation issue

--- data-raw/static_world.R
+++ data-raw/static_world.R
@@ -1,4 +1,5 @@
-vdem <- get_vdem(v2x_libdem, v2x_regime, v2x_accountability, v2x_corr, e_wbgi_gee, e_wbgi_vae) |> area_weighted_synthetic_data(2019)
+vdem <- get_vdem(v2x_libdem, v2x_regime, v2x_accountability, v2x_corr, e_wbgi_gee, e_wbgi_vae, v2regdur) |> area_weighted_synthetic_data(2019)
+vdem2 <- get_vdem(v2regendtype, .fun = min) |> area_weighted_synthetic_data(2019)
 ucdp <- ucdpbrds |> dplyr::select(gwcode, year, best, low, high) |> area_weighted_synthetic_data(2019)
 pwt <- get_ggdc(dataset = "pwt", version = "10.01") |>
   dplyr::select(gwcode, year, rgdpna, rgdpe, rgdpo, pop, emp, cgdpe, cgdpo) |>
@@ -10,20 +11,25 @@ maddison <- get_ggdc(dataset = "maddison", version = "2020") |>
 wcde <- wcde_gwcode |>
   dplyr::rename(wcde_pop = tot_pop)
 
-indicators <- c("wdi_pop" = "SP.POP.TOTL",
-                 "wdi_gdp_pp_con_us" = "NY.GDP.MKTP.PP.KD",
-                 "wdi_gdp_pp_cur_us" = "NY.GDP.MKTP.PP.CD",
+indicators_mean <- c(
                  "wdi_undernourishment" = "SN.ITK.DEFC.ZS",
                  "wdi_imr" = "SP.DYN.IMRT.IN",
                  "wdi_nmr" = "SH.DYN.NMRT")
-wdi <- download_wdi(indicators) |>
+indicators_sum <- c("wdi_pop" = "SP.POP.TOTL",
+                     "wdi_gdp_pp_con_us" = "NY.GDP.MKTP.PP.KD",
+                     "wdi_gdp_pp_cur_us" = "NY.GDP.MKTP.PP.CD")
+wdi <- download_wdi(indicators_sum) |>
+  dplyr::select(-version)
+wdi2 <- download_wdi(indicators_mean, merge_fun = hablar::mean_) |>
   dplyr::select(-version)
 
 
 df <- dplyr::full_join(ucdp, vdem)
+df <- dplyr::left_join(df, vdem2)
 df <- dplyr::left_join(df, maddison, by = c("gwcode", "year"))
 df <- dplyr::left_join(df, pwt, by = c("gwcode", "year"))
 df <- dplyr::left_join(df, wdi, by = c("gwcode", "year"))
+df <- dplyr::left_join(df, wdi2, by = c("gwcode", "year"))
 df <- dplyr::left_join(df, wcde, by = c("gwcode", "year"))
 
 
@@ -107,7 +113,7 @@ df <- df |> dplyr::select(
   gwcode, year,
   rgdp, gdp_grwt, gdppc, gdppc_grwt, population, pop_grwt,
   best, low, high,
-  v2x_libdem, v2x_regime, v2x_accountability, v2x_corr, e_wbgi_gee, e_wbgi_vae,
+  v2x_libdem, v2x_regime, v2x_accountability, v2x_corr, v2regdur, v2regendtype, e_wbgi_gee, e_wbgi_vae,
   priprop, secprop, psecprop, tdr, ydr, odr, youth, working, elderly,
   wdi_undernourishment, wdi_imr, wdi_nmr,
   wcde_pop, pwt_pop, wdi_pop, maddison_pop,
